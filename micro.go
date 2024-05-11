cc07aa5006146710d5cd3237f29ca8a99f2555c5
caixiaorong <caixiaorong@leedarson.com>
2024-05-11 13:47:48 +0800

    add feature

--- micro.go
+++ micro.go
@@ -24,7 +24,7 @@ func (e *Engine) Handle() *web.RequestHandler {
 }
 
 func New() *Engine {
-	trie := web.NewTrie()
+	trie := web.Default()
 	return &Engine{
 		trie:       trie,
 		middleware: make([]web.Handler, 0),
