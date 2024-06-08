f79d48988c52eb5c6cea9ff3da6e2e7910d4e555
caixiaorong <smart_r@163.com>
2024-06-08 20:34:02 +0800

    add db config

--- config/db_cfg.go
+++ config/db_cfg.go
@@ -7,6 +7,29 @@ type mysqlConfig struct {
 	MaxIdleConns int    `yaml:"maxIdleConns"`
 	MaxIdleTime  int64  `yaml:"maxIdleTime"`
 	PrepareStmt  bool   `yaml:"prepareStmt"`
+	// GORM perform single create, update, delete operations in transactions by default to ensure database data integrity
+	// You can disable it by setting `SkipDefaultTransaction` to true
+	SkipDefaultTransaction bool `yaml:"skipDefaultTransaction"`
+	// FullSaveAssociations full save associations
+	FullSaveAssociations bool
+	// DryRun generate sql without execute
+	DryRun bool
+	// DisableAutomaticPing
+	DisableAutomaticPing bool
+	// DisableForeignKeyConstraintWhenMigrating
+	DisableForeignKeyConstraintWhenMigrating bool
+	// IgnoreRelationshipsWhenMigrating
+	IgnoreRelationshipsWhenMigrating bool
+	// DisableNestedTransaction disable nested transaction
+	DisableNestedTransaction bool
+	// AllowGlobalUpdate allow global update
+	AllowGlobalUpdate bool
+	// QueryFields executes the SQL query with all fields of the table
+	QueryFields bool
+	// CreateBatchSize default create batch size
+	CreateBatchSize int
+	// TranslateError enabling error translation
+	TranslateError bool
 }
 
 type dbConfig struct {
