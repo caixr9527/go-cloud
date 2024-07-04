2a049ca7bd33982a51b97b197f560d6ef0013d44
caixiaorong <smart_r@163.com>
2024-07-04 23:30:53 +0800

    add remote transport feature

---
+++ test/service/test_service.go
@@ -0,0 +1,11 @@
+package service
+
+import "github.com/caixr9527/go-cloud/transport"
+
+type GoodsService struct {
+	GetUser func(id int) any
+}
+
+func (g *GoodsService) Client() transport.Client {
+	return transport.Client{}
+}
---
+++ transport/transport_client.go
@@ -0,0 +1,19 @@
+package transport
+
+type Fallback interface {
+}
+type Configuration interface {
+}
+
+type Client struct {
+	Protocol      string
+	Name          string
+	Url           string
+	Path          string
+	Fallback      Fallback
+	Configuration Configuration
+}
+
+type Service interface {
+	Client() Client
+}
